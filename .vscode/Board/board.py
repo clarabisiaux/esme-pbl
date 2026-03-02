bdf79eded046b40e0ffdd095313528adba2d2c64
chloe.allaire <chl1.allaire@gmail.com>
2026-03-02 16:36:36 +0100

    explanations board

--- .vscode/Board/board.py
+++ .vscode/Board/board.py
@@ -1,58 +1,32 @@
 class Board: 
 
     def __init__(self): 
+        self.grid = [" " for _ in range(9)] #create 9 empty boxes
 
-        self.grid = [" " for _ in range(9)] 
-
- 
-
-    def play(self, index, symbol): 
-
+    def play(self, index, symbol): #if the box is empty it puts the symbol in it
         if self.grid[index] == " ": 
-
             self.grid[index] = symbol 
-
             return True 
-
-        return False 
-
- 
+        return False #if the box isn't empty you can't play
 
     def available_moves(self): 
+        return [i for i in range(9) if self.grid[i] == " "] #return all the empty boxes (playable boxes)
 
-        return [i for i in range(9) if self.grid[i] == " "] 
-
- 
-
-    def is_full(self): 
-
+    def is_full(self): #check if the board is full
         return " " not in self.grid 
 
- 
-
     def winner(self): 
-
         lines = [ 
-
             (0,1,2),(3,4,5),(6,7,8), 
-
             (0,3,6),(1,4,7),(2,5,8), 
-
             (0,4,8),(2,4,6) 
-
-        ] 
+        ] #list of winning combinations
 
         for a, b, c in lines: 
+            if self.grid[a] == self.grid[b] == self.grid[c] != " ": #if abc is the same symbol and not empty and part of the winning combinations
+                return self.grid[a] #return the winning symbol (the winner)
+        return None #if there is no winning lines no one wone
 
-            if self.grid[a] == self.grid[b] == self.grid[c] != " ": 
-
-                return self.grid[a] 
-
-        return None 
-
- 
-
-    def reset(self): 
-
+    def reset(self): #recreate an empty grid to restart
         self.grid = [" " for _ in range(9)] 
         
