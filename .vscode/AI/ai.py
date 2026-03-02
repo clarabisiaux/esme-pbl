a3044834dc94ed7fe36b90ef6f3674d92a73d761
queenbaddie <avenas.sarah@gmail.com>
2026-03-02 16:38:05 +0100

    function evaluate ai.py

--- .vscode/AI/ai.py
+++ .vscode/AI/ai.py
@@ -1,5 +1,10 @@
 def evaluate(board, ai_symbol, human_symbol):
-
+  winner=board.winner()
+  if winner == ai_symbol:
+    return 10
+  elif winner ==human_symbol:
+    return -10
+  return 0
   
 
 def minimax(board, depth, maximizing, ai_symbol, human_symbol):
@@ -8,3 +13,4 @@ def minimax(board, depth, maximizing, ai_symbol, human_symbol):
   if score != 0 or board.is_full() or depth==0:
     return score
   
+
