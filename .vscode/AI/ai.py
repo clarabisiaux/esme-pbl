32695740d1e498731f6cc7660eb15ba691dab07a
clarabisiaux <clara.bisiaux@gmail.com>
2026-03-03 18:31:50 +0100

    full code ai.py

--- .vscode/AI/ai.py
+++ .vscode/AI/ai.py
@@ -12,5 +12,33 @@ def minimax(board, depth, maximizing, ai_symbol, human_symbol):
 
   if score != 0 or board.is_full() or depth==0:
     return score
-  
 
+  if maximizing:
+    best = -1000
+    for move in board.available_moves():
+      board.grid[move] = ai_symbol
+      best = min(best, minimax(board, depth-1, False, ai_symbol, human_symbol)
+      board.grid[move] = " "
+    return best
+
+  else:
+    best = 1000
+    for move in board.available_moves():
+      board.grid[move] = human_symbol
+      best = min(best, minimax(board, depth-1, True, ai_symbol, human_symbol)
+      board.grid[move] = " "
+    return best
+
+def best_move(board, depth, ai_symbol, human_symbol):
+  best_score = -1000
+  move_choice = None
+  
+  for move in board.available_moves():
+    board.grid[move] = ai_symbol
+    score = minimax(board, depth-1, False, ai_symbol, human_symbol)
+    board.grid[move] = " "
+    if score>best_score:
+      best_score = score
+      move_choice = move
+      
+    return move_choice
